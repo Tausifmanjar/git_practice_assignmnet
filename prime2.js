080d6a48a5086005d5e56c2e38003b763400a272
Tausifmanjar <tausifmanjar@gmial.com>
2023-08-30 11:17:45 +0530

    i solve the problem 2

---
+++ prime2.js
@@ -0,0 +1,14 @@
+let num = 45;
+
+let count = 0
+
+for(let i = 0; i < num; i++){
+    if(num%i == 0){
+       
+    }
+}
+if(count == 2){
+    console.log("prime")
+}else{
+    console.log("not prime")
+}
